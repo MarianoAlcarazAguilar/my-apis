c768dd67a7f1b51aaddc4d3a7514b632c4c71606
Mariano Alcaraz Aguilar <marianoaaguilar@hotmail.com>
2024-12-18 16:07:04 -0600

    aa

--- ichigo_api.py
+++ ichigo_api.py
@@ -307,7 +307,7 @@ class Ichigo:
             for key, value in equivalencias_calibres.items():
                 if val == value:
                     return key
-            return value / 10_000 
+            return value
         
         if inverse:
             if type(width) != int:
